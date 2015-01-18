c9d940d06a5ab782f093b6e26d923e0b244577a4
Aditya Chaudhary <adityachaudharyfb@gmail.com>
2015-01-19 01:04:20 +0530

    Initial Commit

---
+++ README.txt
@@ -0,0 +1,2 @@
+This is a Personal Contact Display Website written in HTML5, CSS3.
+For live demo goto: http://awesome.pe.hu
---
+++ python/cgi-bin/index.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+
+# enable debugging
+import cgitb
+cgitb.enable()
+
+print "Content-Type: text/plain;charset=utf-8"
+print
+
+print "Hello World!"#!/usr/bin/env python
+
+#import cgi
+#import cgitb; cgitb.enable()  # for troubleshooting
+
+print("Hell")
